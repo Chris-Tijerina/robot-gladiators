e0dde282b0e6b0b3618bd8e5c6e11117e8d88842
Chris T <ctijerina@live.com>
2021-11-08 21:50:33 -0600

    added losing condition message and break - end of lesson 3.2

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,7 +19,6 @@ var enemyAttack = 12;
 var fight = function (enemyName) {
     //repeat and execute as long as the enemy-robot is alive
     while(playerHealth > 0 && enemyHealth > 0) {
-        window.alert("Welcome to Robot Gladiators!");
         // ask player if they'd like to fight or run
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
         
@@ -83,7 +82,24 @@ var fight = function (enemyName) {
     };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        // Let the player know what round they are in, remember that arrays start at 0
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+        
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
